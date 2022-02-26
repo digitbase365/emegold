f04763077d3e4e7a43452ed274d33eef6828e571
unknown <darlsele@gmail.com>
2022-02-26 08:46:06 +0100

    Mobile Nav Drop Menu Fixed

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 const mburger = document.querySelector('header .navbar-toggler') 
 const mobile_menu = document.querySelector('header ul.navbar-nav') 
 const menu_item = document.querySelectorAll('header ul.navbar-nav li a') 
+const nav_div = document.getElementById('navbarSupportedContent')
 
 mburger.addEventListener('click', ()=>{ 
   mobile_menu.classList.remove('activ')
@@ -9,5 +10,6 @@ mburger.addEventListener('click', ()=>{
 menu_item.forEach(item => {
   item.addEventListener('click', ()=>{
   mobile_menu.classList.toggle('activ')
+  nav_div.classList.remove('show')
   })
 })
